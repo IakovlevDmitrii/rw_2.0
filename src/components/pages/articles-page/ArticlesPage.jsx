5336d76ca4611cdfefc0d534c18f1e380237c41a
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2024-01-26 05:11:32 +0600

    fetching article to state

--- src/components/pages/articles-page/ArticlesPage.jsx
+++ src/components/pages/articles-page/ArticlesPage.jsx
@@ -1,59 +1,66 @@
-import React, {useState, useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import Article from "../../article";
-import Spinner from "../../spinner";
-import ErrorIndicator from "../../error-indicator";
-import Pagination from "../../pagination";
-import {requestArticles, changePageNumber} from "./actions";
-import styles from "./ArticlesPage.module.scss";
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Article from '../../article';
+import Spinner from '../../spinner';
+import ErrorIndicator from '../../error-indicator';
+import Pagination from '../../pagination';
+import { requestArticles, changePageNumber } from './actions';
+import styles from './ArticlesPage.module.scss';
 
 function ArticlesPage() {
-    const dispatch = useDispatch();
-    const articlesList = useSelector(state => state.articles.list);
-    const articlesCount = useSelector(state => state.articles.articlesCount);
-    const isFetching = useSelector(state => state.common.isFetching);
-    const currentPage = useSelector(state => state.articles.currentPage);
-    const [hasError, setHasError] = useState(false);
+  const dispatch = useDispatch();
+  const articlesList = useSelector(state => state.articles.list);
+  const articlesCount = useSelector(state => state.articles.articlesCount);
+  const currentPage = useSelector(state => state.articles.currentPage);
+  const [isFetching, setIsFetching] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
-    useEffect(
-       () => {
-           dispatch(requestArticles(5, currentPage))
-               .catch(res => {
-                   if(res.hasError) {
-                       setHasError(true);
-                   }
-               });
+  useEffect(
+    () => {
+      setIsFetching(true);
 
-           return () => {
-               setHasError(false);
-           };
-       }, [currentPage]
-    )
+      dispatch(requestArticles(5, currentPage))
+        .then(res => {
+          if(res.isArticlesReceived) {
+            setIsFetching(false);
+          }
+        })
+        .catch(res => {
+          if(res.hasError) {
+            setHasError(true);
+          }
+        });
 
-    if(hasError) {
-        return <ErrorIndicator errorMessage={"Ошибка при получении данных с сервера"} />
-    }
-    if(isFetching) {
-        return <Spinner />;
-    }
+      return () => {
+        setHasError(false);
+      };
+    }, [currentPage, dispatch]
+  )
 
-    const listToShow = articlesList.map(article => (
-        <Article key={article.slug} content={article} fullSize={false} />
-    ));
+  if(hasError) {
+    return <ErrorIndicator errorMessage="Ошибка при получении данных с сервера" />
+  }
+  if(isFetching) {
+    return <Spinner />;
+  }
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.content}>
-                {listToShow}
+  const listToShow = articlesList.map(article => (
+    <Article key={article.slug} content={article} fullSize={false} />
+  ));
 
-                <Pagination
-                    current={currentPage}
-                    onChange={page => dispatch(changePageNumber(page))}
-                    total={articlesCount}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        {listToShow}
+
+        <Pagination
+          current={currentPage}
+          onChange={page => dispatch(changePageNumber(page))}
+          total={articlesCount}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ArticlesPage;
--- src/store/reducers/common-reducer.js
+++ src/store/reducers/common-reducer.js
@@ -1,66 +1,61 @@
-import {FETCHING_AUTHENTICATION, UPDATE_USER} from "../actions";
-import {LOG_OUT} from "../../components/header/actions";
-import {FETCHING_ARTICLES} from "../../components/pages/articles-page/actions";
-import {FETCHING_ARTICLE} from "../../components/pages/article-page/actions";
-import {FETCHING_ARTICLE_CREATION} from "../../components/pages/new-article-page/actions";
-import {REQUEST_TO_REMOVE_ARTICLE} from "../../components/article/actions";
+import { FETCHING_AUTHENTICATION, UPDATE_USER } from '../actions';
+import { LOG_OUT } from '../../components/header/actions';
+import { FETCHING_ARTICLE } from '../../components/pages/article-page/actions';
+import { FETCHING_ARTICLE_CREATION } from '../../components/pages/new-article-page/actions';
+import { REQUEST_TO_REMOVE_ARTICLE } from '../../components/article/actions';
 
 const initialState = {
-    currentUser: {},
-    isFetching: false,
-    isLoggedIn: false,
+  currentUser: {},
+  isFetching: false,
+  isLoggedIn: false,
 };
 
 // eslint-disable-next-line default-param-last
-export const reducer = (state = initialState, action) => {
-    const { payload, type } = action;
+const commonReducer = (state = initialState, action) => {
+  const { payload, type } = action;
 
-    switch (type) {
-        case FETCHING_ARTICLES:
-            return {
-                ...state,
-                isFetching: payload.status,
-            };
+  switch (type) {
+    case FETCHING_ARTICLE:
+      return {
+        ...state,
+        isFetching: payload.status,
+      };
 
-        case FETCHING_ARTICLE:
-            return {
-                ...state,
-                isFetching: payload.status,
-            };
+    case FETCHING_AUTHENTICATION:
+      return {
+        ...state,
+        isFetching: payload.status,
+      };
 
-        case FETCHING_AUTHENTICATION:
-            return {
-                ...state,
-                isFetching: payload.status,
-            };
+    case FETCHING_ARTICLE_CREATION:
+      return {
+        ...state,
+        isFetching: payload.status,
+      };
 
-        case FETCHING_ARTICLE_CREATION:
-            return {
-                ...state,
-                isFetching: payload.status,
-            };
+    case REQUEST_TO_REMOVE_ARTICLE:
+      return {
+        ...state,
+        isFetching: payload.status,
+      };
 
-        case REQUEST_TO_REMOVE_ARTICLE:
-            return {
-                ...state,
-                isFetching: payload.status,
-            };
+    case LOG_OUT:
+      return {
+        currentUser: {},
+        isFetching: state.isFetching,
+        isLoggedIn: false,
+      }
 
-        case LOG_OUT:
-            return {
-                currentUser: {},
-                isFetching: state.isFetching,
-                isLoggedIn: false,
-            }
+    case UPDATE_USER:
+      return {
+        currentUser: payload.user,
+        isFetching: false,
+        isLoggedIn: true,
+      };
 
-        case UPDATE_USER:
-            return {
-                currentUser: payload.user,
-                isFetching: false,
-                isLoggedIn: true,
-            };
-
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
+
+export default commonReducer;
